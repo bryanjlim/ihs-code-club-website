461621d627e807b5265e1d9560a00bf773714429
Bryan Lim <bryanjlim@outlook.com>
2018-02-23 19:18:29 -0800

    Blog page

---
+++ js/blog.js
@@ -0,0 +1,7 @@
+$(document).ready(
+    function() 
+    {
+        $("#blogWrapper").append("<div class='blog-post-wrapper'><div class='blog-post'><h2 class='blog-post-title'>Another blog post</h2><strong class='d-inline-block mb-2 text-primary'>World</strong><p class='blog-post-meta'>December 23, 2013 by <a href='#'>Jacob</a></p><p>Cum sociis natoque penatibus et magnis <a href='#'>dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p></div></div>");
+
+    }
+);
